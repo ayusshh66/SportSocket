b5563a5e3c15decbde0877ea9197f265f80c1887
ayusshh66 <adonisjefrey66@gmail.com>
2026-08-14 23:27:38 +0530

    made match table in schema

--- backend/src/db/schema.ts
+++ backend/src/db/schema.ts
@@ -1,8 +1,25 @@
-import { integer, pgTable, varchar } from "drizzle-orm/pg-core";
+import { integer, pgEnum, pgTable, text, timestamp, varchar } from "drizzle-orm/pg-core";
 
-export const usersTable = pgTable("users", {
-  id: integer().primaryKey().generatedAlwaysAsIdentity(),
-  name: varchar({ length: 255 }).notNull(),
-  age: integer().notNull(),
-  email: varchar({ length: 255 }).notNull().unique(),
-});
+export const matchStatusEnum = pgEnum("match_status", ["scheduled", "live", "finished"]);
+
+export const matches = pgTable("matches", {
+  id: integer("id").primaryKey(),
+  sports: text("sports").notNull(),
+  homeTeam: text("home_team").notNull(),
+  awayTeam: text("away_team").notNull(),
+  status : matchStatusEnum("status").notNull().default("scheduled"),
+  startTime: timestamp("start_time").notNull(),
+  endTime: timestamp("end_time").notNull(),
+  homeScore : integer("home_score").notNull().default(0),
+  awayScore : integer("away_score").notNull().default(0),
+  createdAt: timestamp("created_at").notNull().defaultNow(),
+  // updatedAt: timestamp("updated_at").notNull().defaultNow(),
+})
+
+export const cometary = pgTable("commentary", {
+  id: integer("id").primaryKey(),
+  matchId: integer("match_id").notNull().references(() => matches.id),
+  minute : integer("minute"),
+  
+
+})
